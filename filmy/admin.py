e6dec5efc6ce10c01ef57c422e6fec399318ea36
Michael Vakula <michael.vakula@student.gyarab.cz>
2024-04-05 09:37:54 +0200

    gheiogh

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from filmy.models import Movie
 
 # Register your models here.
+class MovieAdmin(admin.ModelMovie):
+    pass
+
+admin.site.Register(Movie, MovieAdmin)
--- filmy/models.py
+++ filmy/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=256)
     year = models.PositiveSmallIntegerField(blank= True, null= True)
+    footage = models.PositiveSmallIntegerField(blank=True, null = True, help_text= "in minutes")
+
+    def __str__(self):
+        return self.title
