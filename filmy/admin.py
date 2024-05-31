0349aebbf0600849752248b09a3703182ec851c5
Michael Vakula <michael.vakula@student.gyarab.cz>
2024-05-31 08:24:16 +0200

    commit

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from filmy.models import Movie
+from filmy.models import Movie, Director, Genre, Actor
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
